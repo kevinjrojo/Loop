6579f36832288192806cc8e81183c290395ee8ee
kevin <kevin.rojokevin17@gmail.com>
2025-02-27 20:07:30 -0300

    start iniciado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import { Login } from "./Components/login";
+
+import { Start } from "./Components/Start.jsx";
 
 function App() {
   return (
     <>
-      <Login />
+      {/* <Login /> */}
+      <Start />
     </>
   );
 }
